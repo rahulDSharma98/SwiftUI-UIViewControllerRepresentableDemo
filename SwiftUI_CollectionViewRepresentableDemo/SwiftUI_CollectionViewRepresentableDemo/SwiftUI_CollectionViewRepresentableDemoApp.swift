6cf70c24e498ff283447a2fcde7fd56eba078203
Rahul Sharma <rahul.sharma@tatvasoft.com>
2026-02-26 12:12:16 +0530

    Working on UIViewRepresentable

---
+++ SwiftUI_CollectionViewRepresentableDemo/SwiftUI_CollectionViewRepresentableDemo/SwiftUI_CollectionViewRepresentableDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_CollectionViewRepresentableDemoApp.swift
+//  SwiftUI_CollectionViewRepresentableDemo
+//
+//  Created by MACM62 on 22/01/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_CollectionViewRepresentableDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
